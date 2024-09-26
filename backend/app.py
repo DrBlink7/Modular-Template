cd45ae9d99e61f0bf75b36d7b2cb3f45ae9eb303
Andrea <andrea.manna7@gmail.com>
2024-09-26 13:50:36 +0200

    update python

--- backend/app.py
+++ backend/app.py
@@ -10,4 +10,4 @@ def healthcheck():
     return jsonify({"message": "I'm healthy"}), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8000, debug=True)
