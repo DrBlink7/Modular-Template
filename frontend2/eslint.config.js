dbd225f0d6be845d9c6ba44c973d8ae5dbe102a3
Andrea <andrea.manna7@gmail.com>
2025-01-20 12:51:25 +0100

    chore: update frontend2 vite packages

--- frontend2/eslint.config.js
+++ frontend2/eslint.config.js
@@ -3,26 +3,54 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config(
-  { ignores: ['dist','cypress', 'node_modules'] },
+  { ignores: ['dist', 'cypress', 'node_modules'] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2022,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      '@stylistic': stylistic
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      'comma-dangle': ['error', 'never'],
+      'array-bracket-spacing': ['error', 'never'],
+      'arrow-spacing': ['error', { before: true, after: true }],
+      'block-spacing': ['error', 'always'],
+      'brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      'comma-spacing': ['error', { before: false, after: true }],
+      'comma-style': ['error', 'last'],
+      'computed-property-spacing': ['error', 'never'],
+      'dot-location': ['error', 'property'],
+      'eol-last': ['error', 'always'],
+      'indent': ['error', 2],
+      'key-spacing': ['error', { beforeColon: false, afterColon: true }],
+      'keyword-spacing': ['error', { before: true, after: true }],
+      'lines-between-class-members': ['error', 'always'],
+      'no-mixed-spaces-and-tabs': ['error'],
+      'no-multi-spaces': ['error'],
+      'no-trailing-spaces': ['error'],
+      'object-curly-spacing': ['error', 'always'],
+      'quotes': ['error', 'single', { avoidEscape: true }],
+      'semi': ['error', 'never'],
+      'semi-spacing': ['error', { before: false, after: true }],
+      'space-before-blocks': ['error', 'always'],
+      'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
+      'space-in-parens': ['error', 'never'],
+      'space-infix-ops': ['error'],
+      'space-unary-ops': ['error', { words: true, nonwords: false }]
+    }
+  }
 )
